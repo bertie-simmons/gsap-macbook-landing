60660cb5aa2fdcac0a63ac82d50d2339133fd6a0
bertie <bertiesimmons06@gmail.com>
2026-06-02 11:14:04 +0100

    refactor: navbar items in constants file for better readability

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {navLinks} from "../constants/index"
 
 const NavBar = () => {
   return (
@@ -7,14 +8,7 @@ const NavBar = () => {
         <img src="/logo.svg" alt="Apple logo" />
 
          <ul>
-          {[
-            { label: 'Store' },
-            { label: 'Mac' },
-            { label: 'iPhone' },
-            { label: 'Watch' },
-            { label: 'Vision' },
-            { label: 'AirPods' },
-          ].map(({ label }) => (
+          {navLinks.map(({ label }) => (
             <li key={label}>
               <a href={label}>{label}</a>
             </li>
